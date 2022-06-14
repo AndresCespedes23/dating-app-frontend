30947bfb73b466eafe70e160cdf502500abaf19b
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-06-14 14:54:18 -0300

    added buttons container

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import Header from './Components/Header';
+import SwipeButtons from './Components/Swipe';
 import './reset.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <SwipeButtons />
     </div>
   );
 }
---
+++ src/Components/Swipe/index.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReplayIcon from '@mui/icons-material/Replay';
+import CloseIcon from '@mui/icons-material/Close';
+import StarRateIcon from '@mui/icons-material/StarRate';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FlashOnIcon from '@mui/icons-material/FlashOn';
+import IconButton from '@mui/material/IconButton';
+import styles from './swipe.module.css';
+
+function SwipeButtons() {
+  return (
+    <div className={styles.swipeButtons}>
+      <IconButton className={styles.swipeButtonsRepeat}>
+        <ReplayIcon fontSize="large" />
+      </IconButton>
+      <IconButton className={styles.swipeButtonsLeft}>
+        <CloseIcon fontSize="large" />
+      </IconButton>
+      <IconButton className={styles.swipeButtonsStar}>
+        <StarRateIcon fontSize="large" />
+      </IconButton>
+      <IconButton className={styles.swipeButtonsRight}>
+        <FavoriteIcon fontSize="large" />
+      </IconButton>
+      <IconButton className={styles.swipeButtonsLightning}>
+        <FlashOnIcon fontSize="large" />
+      </IconButton>
+    </div>
+  );
+}
+export default SwipeButtons;
